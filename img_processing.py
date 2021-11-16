660fe5618101d9e6e86aa196ff27425e70e14410
mstryjek <73649466+mstryjek@users.noreply.github.com>
2021-11-16 19:13:37 +0100

    Add hough lines, refine algorithm

--- img_processing.py
+++ img_processing.py
@@ -1,14 +1,14 @@
 import numpy as np
 import cv2
 from math import ceil
+import matplotlib.pyplot as plt
+
 
 
 def thresh(img: np.ndarray, thresh: int = 20) -> np.ndarray:
     """Thresh an image."""
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)[-1]
-
-
 
 
 def cutoff_top_bottom(img: np.ndarray) -> np.ndarray:
@@ -22,22 +22,14 @@ def cutoff_top_bottom(img: np.ndarray) -> np.ndarray:
         if not np.any(img[i]):
             topcnt += 1
             do_break = False
-        
+
         if not np.any(img[h-i-1]):
             bottomcnt += 1
             do_break = False
-    
+
     if topcnt + bottomcnt >= h:
         raise ValueError("Black or empty image passed to function!")
     return img[topcnt:h-bottomcnt]
-
-
-def cutoff_left_right(img: np.ndarray) -> np.ndarray:
-    """
-    Cut off left and/or right black bars from the image.
-    """
-
-
 
 
 def close(img: np.ndarray) -> np.ndarray:
@@ -56,7 +48,7 @@ def blur(img: np.ndarray) -> np.ndarray:
 
 def get_tranform_points(img: np.ndarray) -> np.ndarray:
     """
-    Get warp perspective tranform points 
+    Get warp perspective tranform points
     """
 
 
@@ -65,27 +57,141 @@ def medianblur(img: np.ndarray) -> np.ndarray:
     return cv2.medianBlur(img, 15)
 
 
+def contour(img: np.ndarray) -> np.ndarray:
+    """Get the largest contour in the image."""
+    contours = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+    return max(contours, key=cv2.contourArea)
+
+
+def get_points(contour: np.ndarray) -> np.ndarray:
+    """Get bounding rect of largest contour."""
+    x1, y1, x2, y2 = cv2.boundingRect(contour)
+    return ((x1, y1), (x2, y2))
+
+
+def get_page(img: np.ndarray) -> np.ndarray:
+    """
+    Perform full operation pipeline.
+    """
+    gray = thresh(img)
+    closed = close(gray)
+    blurred = medianblur(closed)
+    cnt = contour(blurred)
+    (xmin, ymin), (xmax, ymax) = get_points(cnt)
+    return img[ymin:ymax, xmin:xmax]
+
+
+def draw_hough_lines(img: np.ndarray, lines: np.ndarray) -> np.ndarray:
+    """Draw detected hough lines on an image."""
+    for rho, theta in lines:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 10000*(-b))
+        y1 = int(y0 + 10000*(a))
+        x2 = int(x0 - 10000*(-b))
+        y2 = int(y0 - 10000*(a))
+
+        img = cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+    return img
+
+
+def get_hough_lines(img: np.ndarray) -> np.ndarray:
+    """
+    Get cleaned parametric version of Hough lines.
+    Retshape is (n,2).
+    """
+    lines = cv2.HoughLines(img, 1, np.pi/180, 300)
+    return np.squeeze(lines)
+
+
+def average_lines(lines: np.ndarray) -> np.ndarray:
+    """
+    Split Hough lines into four edges and average each edge out.
+    """
+    rho_stdev = np.std(lines[:, 0])
+
+    def edge1(lines: np.ndarray) -> np.ndarray:
+        """
+        Edge 1:
+        - small values of rho (around 0)
+        - theta around 0 (<pi/4) or pi (>3pi/4)
+        """
+        small_rho = lines[np.abs(lines[:, 0]) < rho_stdev/2]
+        theta_topi4 = small_rho[small_rho[:, 1] < np.pi/4]
+        theta_from3pi4 = small_rho[small_rho[:, 1] > 3*np.pi/4] - np.pi
+        return np.concatenate([theta_from3pi4, theta_topi4], axis=0)
+
+    def edge2(lines: np.ndarray) -> np.ndarray:
+        """
+        Edge 2:
+        - large values of rho
+        - theta around 0 (<pi/4) or pi (>3pi/4)
+        """
+        large_rho = lines[np.abs(lines[:, 0]) > rho_stdev/2]
+        large_rho[:, 0] = np.abs(large_rho[:, 0])
+        theta_topi4 = large_rho[large_rho[:, 1] < np.pi/4]
+        theta_from3pi4 = large_rho[large_rho[:, 1] > 3*np.pi/4] - np.pi
+        return np.concatenate([theta_from3pi4, theta_topi4], axis=0)
+
+    def edge3(lines: np.ndarray) -> np.ndarray:
+        """
+        Edge 3:
+        - small values of rho (around 0)
+        - theta around pi/2 (>pi/4 and <3pi/4)
+        """
+        large_rho = lines[np.abs(lines[:, 0]) < rho_stdev/2]
+        theta_topi4 = large_rho[large_rho[:, 1] > np.pi/4]
+        theta_from3pi4 = theta_topi4[theta_topi4[:, 1] < 3*np.pi/4]
+        return theta_from3pi4
+
+    def edge4(lines: np.ndarray) -> np.ndarray:
+        """
+        Edge 4:
+        - large values of rho
+        - theta around pi/2 (>pi/4 and <3pi/4)
+        """
+        large_rho = lines[np.abs(lines[:, 0]) > rho_stdev/2]
+        large_rho[:, 0] = np.abs(large_rho[:, 0])
+        theta_topi4 = large_rho[large_rho[:, 1] > np.pi/4]
+        theta_from3pi4 = theta_topi4[theta_topi4[:, 1] < 3*np.pi/4]
+        return theta_from3pi4
+
+    e1_parametric = np.mean(edge1(lines), axis=0)
+    e2_parametric = np.mean(edge2(lines), axis=0)
+    e3_parametric = np.mean(edge3(lines), axis=0)
+    e4_parametric = np.mean(edge4(lines), axis=0)
+
+    return np.stack([e1_parametric, e2_parametric, e3_parametric, e4_parametric],
+            axis=0)
+
+
+def get_intersection_points(lines: np.ndarray) -> np.ndarray:
+    """Get corner points of four lines."""
+    ...
+
+
 """
 Processing path:
 1. Low thresh (global)
-2. Cut off side bars
 3. Closing
-4. Media blur (large `ksize`)
+4. Median blur (large `ksize`)
 5. Get largest area contour
-# 6. [!] `cv2.minAreaRect`
-6. Get corners of max inside rect
-7. Move each corner outwards (keeping a padding distance between corner coords
-and edge). E.g.:
-- move corner to the right until it's 5px from right edge
-- move corner up until it's 5px from top edge 
-- while moving, adjust so that it is constantly 5px from right edge
-- at the end, add 5px to top and right to fund corner
+6. Draw contour
+7. Hough lines
+8. Group lines into four edges
+9. Get average parametric repr of each edge
+10. Get corners as edge intersections
+"""
+
+"""
+TODO
+- Get corner intersection points.
+- Create marching algorithm, fixing the curvature of the scan.
 """
 
 
-img = cv2.imread("closed.jpg")
-img = medianblur(img)
-cv2.imwrite("median.jpg", img)
-
-
-
+img = cv2.imread("CNT.jpg", cv2.IMREAD_GRAYSCALE)
+cv2.imwrite("lines.jpg", img)
